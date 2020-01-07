968acf96e022fb248f91c2b6226df7e33e95a543
Marian Horodnic <horodnicm7@gmail.com>
2020-01-07 20:48:29 +0200

    Added video capture from camera for file and key input

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+
+from src.tracker import Tracker
+
+
+parser = argparse.ArgumentParser(description='Mosse object tracking')
+parser.add_argument('-f', '--file', type=str, required=False,
+                    help='Path to video file. If not specified, it will use the camera')
+parser.add_argument('-w', '--width', type=int, required=False,
+                    help='Width of displayed video')
+args = parser.parse_args()
+
+
+def main():
+    port = args.file if args.file else 0
+    tracker = Tracker(port)
+    tracker.run()
+
+
+if __name__ == '__main__':
+    main()
