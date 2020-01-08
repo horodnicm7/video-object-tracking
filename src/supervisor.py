5bfb601ba2603b8e8ee680e94ac10b5f9d03b96d
Marian Horodnic <horodnicm7@gmail.com>
2020-01-08 11:48:33 +0200

    on mouse move event works now

--- src/supervisor.py
+++ src/supervisor.py
@@ -5,15 +5,17 @@ from src.mosse import Mosse
 
 
 class Supervisor(object):
-    window_name = 'mosse tracker'
-
-    def __init__(self, video):
+    def __init__(self, video, window_name='mosse_tracker'):
+        self.window_name = window_name
         self.file_path = video
         self.video = cv2.VideoCapture(video)
         self.running = True
         self.paused = False
         self.trackers = []
         self.current_frame = None
+
+        self.start_points = None
+        self.rectangle = None
 
     @property
     def gray_frame(self):
@@ -22,7 +24,32 @@ class Supervisor(object):
     def __on_select(self, selection):
         self.trackers.append(Mosse(self.gray_frame, selection))
 
+    def on_mouse_move(self, event, x, y, flags, _):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.start_points = (x, y)
+        elif self.start_points:
+            # if it's a click release, then there is a selection
+            if flags & cv2.EVENT_FLAG_LBUTTON:
+                x0, y0 = [min(x, y) for x, y in zip(self.start_points, (x, y))]
+                x1, y1 = [max(x, y) for x, y in zip(self.start_points, (x, y))]
+
+                # if the selection is larger than a point and not a straight line
+                if x1 > x0 and y1 > y0:
+                    self.rectangle = (x0, y0, x1, y1)
+
+                return
+
+            if self.rectangle:
+                self.__on_select(self.rectangle)
+            self.rectangle = None
+            self.start_points = None
+
     def run(self, width=500):
+        self.current_frame = self.video.read()[1]
+        cv2.imshow(self.window_name, self.current_frame)
+
+        cv2.setMouseCallback(self.window_name, self.on_mouse_move)
+
         while self.running:
             if not self.paused:
                 frame = self.video.read()[1]
@@ -36,9 +63,9 @@ class Supervisor(object):
                     tracker.update(self.gray_frame)
 
                 frame = imutils.resize(frame, width=width)
-                cv2.imshow(Supervisor.window_name, frame)
+                cv2.imshow(self.window_name, frame)
 
-            key = cv2.waitKey(10) & 0xFF
+            key = cv2.waitKey(10)
 
             if key == ord('q'):
                 break
