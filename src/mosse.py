c7e26e12a90f4192461c09c9e2b601e9e46a4c76
Marian Horodnic <horodnicm7@gmail.com>
2020-01-10 13:04:44 +0200

    Almost done

--- src/mosse.py
+++ src/mosse.py
@@ -11,23 +11,86 @@ class Mosse(object):
 
         self.template = cv2.getRectSubPix(initial_frame, self.size, self.template_center)
         self.__prepare_convolution_terms()
+        self.update_kernel()
+        self.update(initial_frame)
 
-    def preprocess_frame(self, frame, ):
+    @staticmethod
+    def rnd_warp(a):
+        h, w = a.shape[:2]
+        T = np.zeros((2, 3))
+        coef = 0.2
+        ang = (np.random.rand() - 0.5) * coef
+        c, s = np.cos(ang), np.sin(ang)
+        T[:2, :2] = [[c, -s], [s, c]]
+        T[:2, :2] += (np.random.rand(2, 2) - 0.5) * coef
+        c = (w / 2, h / 2)
+        T[:, 2] = c - np.dot(T[:2, :2], c)
+        return cv2.warpAffine(a, T, (w, h), borderMode=cv2.BORDER_REFLECT)
+
+    @staticmethod
+    def divSpec(A, B):
+        Ar, Ai = A[..., 0], A[..., 1]
+        Br, Bi = B[..., 0], B[..., 1]
+        C = (Ar + 1j * Ai) / (Br + 1j * Bi)
+        C = np.dstack([np.real(C), np.imag(C)]).copy()
+        return C
+
+    def update_kernel(self):
+        self.H = Mosse.divSpec(self.H1, self.H2)
+        self.H[..., 1] *= -1
+
+    def correlate(self, img):
+        C = cv2.mulSpectrums(cv2.dft(img, flags=cv2.DFT_COMPLEX_OUTPUT), self.H, 0, conjB=True)
+        resp = cv2.idft(C, flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+        h, w = resp.shape
+        _, mval, _, (mx, my) = cv2.minMaxLoc(resp)
+        side_resp = resp.copy()
+        cv2.rectangle(side_resp, (mx - 5, my - 5), (mx + 5, my + 5), 0, -1)
+        smean, sstd = side_resp.mean(), side_resp.std()
+        psr = (mval - smean) / (sstd + Mosse.EPS)
+        return resp, (mx - w // 2, my - h // 2), psr
+
+    def update(self, frame, learning_rate=0.125):
+        # get the current's frame template and preprocess it
+        image = cv2.getRectSubPix(frame, self.size, self.template_center)
+        image = self.preprocess_frame(image)
+
+        self.last_resp, (dx, dy), self.psr = self.correlate(image)
+
+        # PSR under 8 means that the object is occluded or tracking has failed
+        if self.psr < 8.0:
+            return
+
+        # update the templates center according to the new discovered position
+        self.template_center = (self.template_center[0] + dx, self.template_center[1] + dy)
+        image = cv2.getRectSubPix(frame, self.size, self.template_center)
+        image = self.preprocess_frame(image)
+
+        image_dft = cv2.dft(image, flags=cv2.DFT_COMPLEX_OUTPUT)
+        H1 = cv2.mulSpectrums(self.G, image_dft, 0, conjB=True)
+        H2 = cv2.mulSpectrums(image_dft, image_dft, 0, conjB=True)
+
+        self.H1 = H1 * learning_rate + self.H1 * (1.0 - learning_rate)  # equation 11
+        self.H2 = H2 * learning_rate + self.H2 * (1.0 - learning_rate)  # equation 12
+
+        self.update_kernel()
+
+    def preprocess_frame(self, frame):
         # the pixel values are transformed using a log function which helps with low contrast
         # lighting situations.
         image = np.log(np.float32(frame) + 1.0)
 
         # the pixel values are normalized to have a mean value of 0.0 and a norm of 1.0
         image = (image - image.mean()) / (image.std() + Mosse.EPS)
-        return image * self.template
+        return image * self.hann_window
 
     def __prepare_convolution_terms(self):
-        self.G = cv2.dft(g, flags=cv2.DFT_COMPLEX_OUTPUT)
+        self.G = cv2.dft(self.g, flags=cv2.DFT_COMPLEX_OUTPUT)
         self.H1 = np.zeros(shape=self.G.shape)
         self.H2 = np.zeros_like(shape=self.G.shape)
 
         for _ in range(128):
-            a = self.preprocess(rnd_warp(self.template))
+            a = self.preprocess_frame(Mosse.rnd_warp(self.template))
 
             # compute the DFT of the image
             A = cv2.dft(a, flags=cv2.DFT_COMPLEX_OUTPUT)
@@ -43,7 +106,9 @@ class Mosse(object):
         self.g = np.zeros((self.size[::-1]), np.float32)
         self.g[self.size[1] // 2, self.size[0] // 2] = 1
         self.g = cv2.GaussianBlur(self.g, (-1, -1), 2.0)
-        self.g /= g.max()
+        self.g /= self.g.max()
+
+        self.hann_window =  cv2.createHanningWindow(self.size, cv2.CV_32F)
 
     def __get_template_info(self, initial_frame, rectangle):
         left_x, left_y, right_x, right_y = rectangle
@@ -60,7 +125,17 @@ class Mosse(object):
         self.size = (width, height)
 
     def display_selection(self, frame):
-        pass
+        left_x = int(self.template_center[0] - 0.5 * self.size[0])
+        left_y = int(self.template_center[1] - 0.5 * self.size[1])
+        right_x = int(self.template_center[0] + 0.5 * self.size[0])
+        right_y = int(self.template_center[1] + 0.5 * self.size[1])
 
-    def update(self, frame):
-        pass
+        cv2.rectangle(frame, (left_x, left_y), (right_x, right_y), (0, 0, 255))
+
+        text = 'PSR: {}'.format(self.psr)
+        cv2.putText(frame, text, (left_x + 1, right_y + 1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+        cv2.putText(frame, text, (left_x, right_y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
+
+        if self.psr < 8.0:
+            cv2.line(frame, (left_x, left_y), (right_x, right_y), (0, 0, 255))
+            cv2.line(frame, (right_x, left_y), (left_x, right_y), (0, 0, 255))
